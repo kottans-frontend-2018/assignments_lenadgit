457e4d029179aa722d00900106f75702e5f87126
lenadgit <lena.boruslav@gmail.com>
2018-04-24 21:12:43 +0300

    upd-24-04

--- 13/src/pizza-data.js
+++ 13/src/pizza-data.js
@@ -2,19 +2,11 @@ import {AUTH_HTTP} from './auth.http'
 import {INGREDIENTS_URL} from './constants'
 import {PizzaComponent} from './pizza.component'
 
-	class pizzaDataService() {
+		class pizzaDataService() {
 		constructor() {
 			this.ingredients = [];
 			this.tags = [];
-
-
-	onInit() {
-		const node = document.querySelector("#data-pizza")
-		PIZZA_DATA.getIngredients().then(ingredients => console.log(ingredients))
-		}
-	document.querySelector('.add-btn').addEventListener('click', onInit, false);
-
-			
+			document.querySelector('.add-btn').addEventListener('click', onInit, false);
 		}
 			getIngredients() {
 				return AUTH_HTTP.get('INGREDIENTS_URL').then(
