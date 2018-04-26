4cb888c3452b29250f77194904aea61f4db9edcf
lenadgit <lena.boruslav@gmail.com>
2018-04-26 22:03:29 +0300

    26-04-22-10

--- 13/src/pizza.component.js
+++ 13/src/pizza.component.js
@@ -1,12 +1,14 @@
 import {PIZZA_DATA} from './pizza-data';
 
-export class PizzaComponent { 
-	onInit() {
-		const node = document.querySelector("#data-pizza")
-		PIZZA_DATA.getIngredients().then(ingredients => console.log(ingredients))
-	}
-	document.querySelector('.add-btn').addEventListener('click', onInit, false);
-}
+export class PizzaComponent {
+    constructor() {
+              document.querySelector('.add-btn').addEventListener('click', this.onInit, false);
+    }
+    onInit() {
+        const node = document.querySelector("#data-pizza")
+        PIZZA_DATA.getIngredients().then(ingredients => console.log(ingredients))
+    }
 
+}
 
 
