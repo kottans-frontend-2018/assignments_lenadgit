ac5a6f59cc38f87b9791e121a10fa653efd8ac82
lenadgit <lena.boruslav@gmail.com>
2018-04-26 22:00:31 +0300

    26-04-22-00

--- 13/src/index.js
+++ 13/src/index.js
@@ -2,19 +2,15 @@ import {AUTH_HTTP} from "./auth.http";
 import {BASE_URL, INGREDIENTS_URL, TAG_URL} from './constants';
 import {PIZZA_DATA} from './pizza-data';
 import PizzaComponent from "./pizza.component";
-import pizzaDataComponent from "./init";
-
-
-
-
+//import pizzaDataComponent from "./init";
 
 //const pizzadata = new PIZZA_DATA();
-const component = new pizzaDataComponent();
+//const component = new pizzaDataComponent();
 const pizzacomp = new PizzaComponent();
 //const PIZZA_DATA = new pizzaDataService();
 
 
-
+pizzacomp.render();
 pizzadata.render();
 component.render();
 
