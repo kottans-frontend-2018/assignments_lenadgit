d1552ec22db16ce8566b30584f42eebfb57917d5
lenadgit <lena.boruslav@gmail.com>
2018-04-26 10:09:14 +0300

    26-04

--- 13/src/pizza.component.js
+++ 13/src/pizza.component.js
@@ -1,35 +1,11 @@
 import {PIZZA_DATA} from './pizza-data';
 
-export class PizzaComponent { // extends Component {
+export class PizzaComponent { 
 	onInit() {
 		const node = document.querySelector("#data-pizza")
 		PIZZA_DATA.getIngredients().then(ingredients => console.log(ingredients))
-		//Promise.all([PIZZA_DATA.getIngredients(), PIZZA_DATA.getTags()])
-		//.then(data => console.log(data[0], data[1]))
-		//.then(() => {
-		//	PIZZA_DATA.ingredients
-		//	PIZZA_DATA.tags
-
-		//	let html = `
-		//	<form>
-		//	<input type = 'text' name = 'name'>
-		//	<br>
-		//	<label>Ingredients</label>
-		//	<div>
-		//	${PIZZA_DATA.ingredietns.reduce((html, ingr) => {
-		//		html +=
-		//		<label>
-		//		${ingr.name}
-		//		<input type = 'checkbox'>
-		//		</label>
-		//		return html
-		//	}, ''}
-		//	</div>
-		//	</form>
-		//	`
-		//}
 	}
-	//document.querySelector('.add-btn').addEventListener('click', onInit, false);
+	document.querySelector('.add-btn').addEventListener('click', onInit, false);
 }
 
 
